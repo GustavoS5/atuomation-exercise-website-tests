b0a119470dfbb614a4fdc30dffa3afeb2be3b867
Berilium <jgusta.s25@gmail.com>
2026-06-30 22:46:50 -0300

    refactor: optimize CartModal properties initialization

--- pages/cart_modal.py
+++ pages/cart_modal.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from playwright.sync_api import Locator, Page
+from playwright.sync_api import Page
 
 
 class CartModal:
@@ -10,26 +10,14 @@ class CartModal:
 
     def __init__(self, page: Page) -> None:
         self.page = page
-
-    @property
-    def modal(self) -> Locator:
-        return self.page.locator("#cartModal")
-
-    @property
-    def title(self) -> Locator:
-        return self.modal.get_by_text("Added!")
-
-    @property
-    def message(self) -> Locator:
-        return self.modal.get_by_text("Your product has been added to cart.")
-
-    @property
-    def view_cart_link(self) -> Locator:
-        return self.modal.locator("a[href='/view_cart']")
-
-    @property
-    def continue_shopping_button(self) -> Locator:
-        return self.modal.get_by_role("button", name="Continue Shopping")
+        self.modal = page.locator("#cartModal")
+        # Dependent locators are assigned after their parent so the chain holds.
+        self.title = self.modal.get_by_text("Added!")
+        self.message = self.modal.get_by_text("Your product has been added to cart.")
+        self.view_cart_link = self.modal.locator("a[href='/view_cart']")
+        self.continue_shopping_button = self.modal.get_by_role(
+            "button", name="Continue Shopping"
+        )
 
     def wait_until_visible(self) -> None:
         """Wait until the modal is visible after adding a product."""
